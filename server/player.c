dd9f8adc00f6f514d5ea54df13572c377c2b0cc6
cvs <cvs@282e977c-c81d-0410-88c4-b93c2d0d6712>
2000-03-27 06:25:55 +0000

    server/player.c:  If player is using pickup mode 6, only have them pick up the item if they know it is magical.  Prior to change, items would be picked up if they are magical, but not if the player knew that, so the code was giving extra knowledge to players.  Bug reported by Kurt Fitzner, checked in by Mark Wedel, 3/26/2000
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/trunk/crossfire@185 282e977c-c81d-0410-88c4-b93c2d0d6712

--- server/player.c
+++ server/player.c
@@ -6,7 +6,7 @@
 /*
     CrossFire, A Multiplayer game for X-windows
 
-    Copyright (C) 1994 Mark Wedel
+    Copyright (C) 2000 Mark Wedel
     Copyright (C) 1992 Frank Tore Johansen
 
     This program is free software; you can redistribute it and/or modify
@@ -23,7 +23,7 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-    The author can be reached via e-mail to master@rahul.net
+    The author can be reached via e-mail to mwedel@scruz.net
 */
 
 #include <pwd.h>
@@ -841,7 +841,7 @@ int check_pick(object *op) {
 		temp = item->below;
 		if (can_pick(op, item)) {
 			if (op->contr->mode==6) {
-			    if (is_magical(item) &&
+			    if (QUERY_FLAG(item, FLAG_KNOWN_MAGICAL) &&
 			      !QUERY_FLAG(item, FLAG_KNOWN_CURSED)) {
 				pick_up(op, item);
 			    }
