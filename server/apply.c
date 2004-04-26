9a9550ff1bb417fd169e1aa734aa32cf6095434e
mwedel <mwedel@282e977c-c81d-0410-88c4-b93c2d0d6712>
2004-04-26 05:27:36 +0000

    server/apply.c: apply_savebed() - make call to terminate_all pets before    call to remove_ob() so that pets are properly removed.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/trunk/crossfire@2744 282e977c-c81d-0410-88c4-b93c2d0d6712

--- server/apply.c
+++ server/apply.c
@@ -2047,6 +2047,8 @@ static void apply_savebed (object *pl)
       new_draw_info(NDI_UNIQUE, 0,pl,"Since you have cheated you can't save.");
       return;
     }
+    /* Need to call terminate_all_pets()  before we remove the player ob */
+    terminate_all_pets(pl);
     remove_ob(pl);
     pl->direction=0;
     new_draw_info_format(NDI_UNIQUE | NDI_ALL | NDI_DK_ORANGE, 5, pl,
