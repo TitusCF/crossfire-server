c94d306929a98a12512190118ebaece2fd4ab30a
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2005-12-07 21:02:24 +0000

    server/ban.c: General cleanup - whitespace changes, rename variable names.    Should not affect function.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/trunk/crossfire@4099 282e977c-c81d-0410-88c4-b93c2d0d6712

--- server/ban.c
+++ server/ban.c
@@ -5,10 +5,10 @@
 
 /*
  * Ban.c
- * Code was grabbed from the netrek source and modified to work with 
+ * Code was grabbed from the netrek source and modified to work with
  * crossfire. This function checks a file in the lib directory for any
  * banned players. If it finds one it returns a 1. Wildcards can be used.
- */ 
+ */
 
 #include <global.h>
 #include <sproto.h>
@@ -36,106 +36,122 @@
  */
 int checkbanned(const char *login, const char *host)
 {
-  FILE  *bannedfile;
-  char  buf[MAX_BUF];
-  char  log_buf0[160], host_buf[64], line_buf[160];
-  char  *indexpos;
-  int   num1;
-  int   Hits=0;                 /* Hits==2 means we're banned */
-  int	loop=0;
-  
-  /* Inverse ban feature: if a line is prefixed by a ~, then we will
-   * immediately return "check passed" if it matches. This allow to ban
-   * a network, but let a part of it still connect.
-   */
-  int   inverse_ban = 0;
+    FILE *bannedfile;
+    char buf[MAX_BUF];
+    char log_buf0[160], host_buf[64], line_buf[160];
+    char *indexpos;
+    int num1;
+    int hits = 0;               /* Hits==2 means we're banned */
+    int loop;
 
- while (loop < 2) {	/* have to check both ban files now */
+    /* Inverse ban feature: if a line is prefixed by a ~, then we will
+     * immediately return "check passed" if it matches. This allow to ban a
+     * network, but let a part of it still connect.
+     */
+    int inverse_ban = 0;
 
-		/* First time through look for BANFILE */
+    for (loop = 0; loop < 2; loop++) {  /* have to check both ban files now */
 
-	 if (loop == 0){	
-  	 sprintf (buf, "%s/%s", settings.confdir, BANFILE);
-  		if ((bannedfile = fopen(buf, "r")) == NULL) {
-    	LOG (llevDebug, "Could not find file Banned file\n");
-    	loop++;	
-  		}
-	}
-		
-		/* Second time through look for BANISHFILE */
+        /* First time through look for BANFILE */
 
-  	if (loop == 1){
-  sprintf (buf, "%s/%s", settings.localdir, BANISHFILE);
-		if ((bannedfile = fopen(buf, "r")) == NULL) {
-    	LOG (llevDebug, "Could not find file Banish file\n");
-    	return(0);
-  		}
-	}
+        if (loop == 0) {
+            sprintf(buf, "%s/%s", settings.confdir, BANFILE);
+            bannedfile = fopen(buf, "r");
+            if (bannedfile == NULL) {
+                LOG(llevDebug, "Could not find file Banned file\n");
+                loop++;
+            }
+        }
 
-	/* Do the actual work here checking for banned IPs */
-  
-  while(fgets(line_buf, 160, bannedfile) != NULL) {
-    char *log_buf = log_buf0;
+        /* Second time through look for BANISHFILE */
 
-    inverse_ban = 0;
-    Hits = 0;
+        if (loop == 1) {
+            sprintf(buf, "%s/%s", settings.localdir, BANISHFILE);
+            bannedfile = fopen(buf, "r");
+            if (bannedfile == NULL) {
+                LOG(llevDebug, "Could not find file Banish file\n");
+                return(0);
+            }
+        }
 
-    /* Split line up */
-    if((*line_buf=='#')||(*line_buf=='\n'))
-      continue;
-    if ((indexpos = (char *) strrchr(line_buf, '@')) == 0) {
-      LOG (llevDebug, "Bad line in banned file\n");
-      continue;
+        /* Do the actual work here checking for banned IPs */
+
+        while (fgets(line_buf, 160, bannedfile) != NULL) {
+            char *log_buf = log_buf0;
+
+            inverse_ban = 0;
+            hits = 0;
+
+            /* Split line up */
+            if(*line_buf == '#' || *line_buf == '\n')
+                continue;
+
+            indexpos = strrchr(line_buf, '@');
+            if (indexpos == NULL) {
+                LOG(llevDebug, "Bad line in banned file\n");
+                continue;
+            }
+
+            /* copy login name into log_buf */
+            num1 = indexpos-line_buf;
+            strncpy(log_buf, line_buf, num1);
+            log_buf[num1] = '\0';
+
+            /* copy host name into host_buf */
+            strncpy(host_buf, indexpos+1, sizeof(host_buf)-1);
+            host_buf[sizeof(host_buf)-1] = '\0';
+
+            /* Cut off any extra spaces on the host buffer */
+            indexpos = host_buf;
+            while (!isspace(*indexpos)) {
+                indexpos++;
+            }
+            *indexpos = '\0';
+
+            if (*log_buf == '~') {
+                log_buf++;
+                inverse_ban = 1;
+            }
+
+            /*
+              LOG(llevDebug, "Login: <%s>; host: <%s>\n", login, host);
+              LOG(llevDebug, "    Checking Banned <%s> and <%s>.\n", log_buf, host_buf);
+            */
+
+            if(*log_buf=='*')
+                hits = 1;
+            else if (login != NULL && strcmp(login, log_buf) == 0)
+                hits = 1;
+
+            if (hits == 1) {
+                if (*host_buf == '*') { /* Lock out any host */
+                    hits++;
+
+                    /* break out now. otherwise hits will get reset to one */
+                    break;
+                }
+                else if (strstr(host, host_buf) != NULL) { /* Lock out subdomains (eg, "*@usc.edu" */
+                    hits++;
+
+                    /* break out now. otherwise hits will get reset to one */
+                    break;
+                }
+                else if (strcmp(host, host_buf) == 0) { /* Lock out specific host */
+                    hits++;
+
+                    /* break out now. otherwise hits will get reset to one */
+                    break;
+                }
+            }
+        }
+        fclose(bannedfile);
+
+        if (hits >= 2) {
+            return(!inverse_ban);
+        }
+
+        loop++;
     }
-    num1 = indexpos - line_buf;
-    strncpy(log_buf, line_buf, num1); /* copy login name into log_buf */
-    log_buf[num1] = '\0';
-    strncpy(host_buf, indexpos + 1, 64); /* copy host name into host_buf */
-    host_buf[63] = '\0';
-    /* Cut off any extra spaces on the host buffer */
-    indexpos = host_buf;
-    while (!isspace(*indexpos))
-      indexpos++;
-    *indexpos = '\0';
 
-    if (*log_buf == '~') {
-      log_buf++;
-      inverse_ban = 1;
-    }
-
-    /*
-      LOG (llevDebug, "Login: <%s>; host: <%s>\n", login, host);
-      LOG (llevDebug, "    Checking Banned <%s> and <%s>.\n",log_buf,host_buf);
-    */
-
-    if(*log_buf=='*')
-
-      Hits=1;
-    else if (login != NULL && strcmp(login, log_buf) == 0)
-      Hits=1;
-    if(Hits==1)
-      {
-        if (*host_buf == '*'){  /* Lock out any host */
-          Hits++;
-          break;                /* break out now. otherwise Hits will get reset
-                                   to one */
-        }
-        else if(strstr(host,host_buf)!=NULL){ /* Lock out subdomains (eg, "*@usc.edu" */
-          Hits++;
-          break;                /* break out now. otherwise Hits will get reset
-                                   to one */
-        }
-        else if (!strcmp(host, host_buf)){ /* Lock out specific host */
-          Hits++;
-          break;                /* break out now. otherwise Hits will get reset
-                                   to one */
-        }
-      }
-  }
-  fclose(bannedfile);
-  if(Hits>=2)
-    return(!inverse_ban);
-  loop++;  
- }
-	return(0);
+    return(0);
 }
