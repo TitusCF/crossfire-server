2c66772ce09455c8e0f096d3d994191ecbb6ea47
tchize <tchize@282e977c-c81d-0410-88c4-b93c2d0d6712>
2006-03-21 22:46:09 +0000

    Initial commit of unit tests (see doc/Developers/testplans) This is it, initial commit of unit test framework. It contains empty testcases for all unit tests. See test/unit/check_shstr.c for a completed example unit test :)
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/trunk/crossfire@4543 282e977c-c81d-0410-88c4-b93c2d0d6712

---
+++ test/unit/common/stubs_common.c
@@ -0,0 +1,88 @@
+/**
+ * Those are dummy functions defined to resolve all symboles.
+ * Added as part of glue cleaning.
+ * Ryo 2005-07-15
+ **/
+#include <global.h>
+
+void fix_auto_apply(mapstruct * m)
+{
+}
+
+void new_draw_info(int a, int b, const object *ob, const char *txt)
+    {
+    fprintf(logfile, "%s\n", txt);
+    }
+void new_info_map(int color, mapstruct *map, const char *str)
+    {
+    fprintf(logfile, "new_info_map: %s\n", str);
+    }
+ 
+void move_teleporter( object* ob)
+    {
+    }
+ 
+void move_firewall( object* ob)
+    {
+    }
+ 
+void move_duplicator( object* ob)
+    {
+    }
+ 
+void move_marker( object* ob)
+    {
+    }
+ 
+void move_creator( object* ob)
+    {
+    }
+ 
+void emergency_save( int x )
+    {
+    }
+ 
+void clean_tmp_files( )
+    {
+    }
+ 
+void esrv_send_item( object* ob, object* obx )
+    {
+    }
+ 
+void dragon_ability_gain( object* ob, int x, int y )
+    {
+    }
+ 
+void weather_effect( const char* c )
+    {
+    }
+ 
+void set_darkness_map( mapstruct* m)
+    {
+    }
+ 
+void move_apply( object* ob, object* obt, object* obx )
+    {
+    }
+ 
+object* find_skill_by_number( object* ob, int x )
+    {
+    return NULL;
+    }
+ 
+void esrv_del_item(player *pl, int tag)
+    {
+    }
+
+void esrv_update_spells(player *pl)
+    {
+    }
+ 
+void monster_check_apply( object* ob, object* obt )
+    {
+    }
+ 
+void trap_adjust( object* ob, int x )
+    {
+    }
---
+++ test/unit/socket/stubs_socket.c
@@ -0,0 +1,79 @@
+/**
+ * Those are dummy functions defined to resolve all symboles.
+ * Added as part of glue cleaning.
+ * Ryo 2005-07-15
+ **/
+#include <global.h>
+
+void fix_auto_apply(mapstruct * m)
+{
+}
+
+void move_teleporter( object* ob)
+    {
+    }
+ 
+void move_firewall( object* ob)
+    {
+    }
+ 
+void move_duplicator( object* ob)
+    {
+    }
+ 
+void move_marker( object* ob)
+    {
+    }
+ 
+void move_creator( object* ob)
+    {
+    }
+ 
+void emergency_save( int x )
+    {
+    }
+ 
+void clean_tmp_files( )
+    {
+    }
+ 
+void esrv_send_item( object* ob, object* obx )
+    {
+    }
+ 
+void dragon_ability_gain( object* ob, int x, int y )
+    {
+    }
+ 
+void weather_effect( const char* c )
+    {
+    }
+ 
+void set_darkness_map( mapstruct* m)
+    {
+    }
+ 
+void move_apply( object* ob, object* obt, object* obx )
+    {
+    }
+ 
+object* find_skill_by_number( object* ob, int x )
+    {
+    return NULL;
+    }
+ 
+void esrv_del_item(player *pl, int tag)
+    {
+    }
+
+void esrv_update_spells(player *pl)
+    {
+    }
+ 
+void monster_check_apply( object* ob, object* obt )
+    {
+    }
+ 
+void trap_adjust( object* ob, int x )
+    {
+    }
