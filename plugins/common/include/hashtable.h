8cba7f5f2bc43bb81e1072f7edd1e65d494167b0
qal21 <qal21@282e977c-c81d-0410-88c4-b93c2d0d6712>
2006-07-29 07:12:05 +0000

    Add hashtable.c for planned use in cfpython.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/trunk/crossfire@4722 282e977c-c81d-0410-88c4-b93c2d0d6712

---
+++ plugins/common/include/hashtable.h
@@ -0,0 +1,27 @@
+/* Usually we will store only about 10 or so elements, however we may get
+ * cases of a fair bit more and we should handle those cases efficently.
+ * hashptr() assumes this value will fit in int.
+ */
+#define PTR_ASSOC_TABLESIZE 251
+
+/* The offsetof macro is part of ANSI C, but many compilers lack it, for
+ * example "gcc -ansi"
+ */
+#if !defined (offsetof)
+#define offsetof(type, member) (int)&(((type *)0)->member)
+#endif
+
+typedef struct _ptr_assoc {
+    struct _ptr_assoc **array;
+    struct _ptr_assoc *previous;
+    struct _ptr_assoc *next;
+    void *key;
+    void *value;
+} ptr_assoc;
+
+typedef ptr_assoc *ptr_assoc_table[PTR_ASSOC_TABLESIZE];
+
+extern void init_ptr_assoc_table(ptr_assoc **hash_table);
+extern void add_ptr_assoc(ptr_assoc **hash_table, void *key, void *value);
+extern void *find_assoc_value(ptr_assoc **hash_table, void *key);
+extern void free_ptr_assoc(ptr_assoc **hash_table, void *key);
