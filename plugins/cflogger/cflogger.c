9a63c26c54c1f34a7f8dff25a2ee0af93d0d1c3e
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2007-01-29 22:05:54 +0000

    Whitespace changes.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@5354 282e977c-c81d-0410-88c4-b93c2d0d6712

--- plugins/cflogger/cflogger.c
+++ plugins/cflogger/cflogger.c
@@ -23,7 +23,8 @@
 /*  You should have received a copy of the GNU General Public License        */
 /*  along with this program; if not, write to the Free Software              */
 /*  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                */
-/*                                                                           */ /*****************************************************************************/
+/*                                                                           */
+/*****************************************************************************/
 
 /**
  * @file cflogger.c
@@ -576,4 +577,3 @@ int closePlugin()
     }
     return 0;
 }
-
