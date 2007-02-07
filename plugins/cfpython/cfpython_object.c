a4ffe95905ae8dde1a150f747f3e3e60ac120cc2
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2007-02-07 01:08:37 +0000

    Whitespace change.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@5393 282e977c-c81d-0410-88c4-b93c2d0d6712

--- plugins/cfpython/cfpython_object.c
+++ plugins/cfpython/cfpython_object.c
@@ -24,7 +24,9 @@
 /*  You should have received a copy of the GNU General Public License        */
 /*  along with this program; if not, write to the Free Software              */
 /*  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                */
-/*                                                                           */ /*****************************************************************************/
+/*                                                                           */
+/*****************************************************************************/
+
 #include <cfpython.h>
 #include <cfpython_object_private.h>
 #include <hashtable.h>
