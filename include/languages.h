8b5b610b5d9d5c6f7ce7c9386a96255e94ca6ce0
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2008-03-02 08:39:27 +0000

    Fix incorrect i18n message; reverts part of r8486.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@8487 282e977c-c81d-0410-88c4-b93c2d0d6712

--- include/languages.h
+++ include/languages.h
@@ -48,7 +48,7 @@ extern const char* i18n_strings[NUM_LANGUAGES][NUM_I18N_STRINGS]; /**< Translati
 #define I18N_MSG_CMISC_009 9
 #define I18N_MSG_CMISC_010 10
 #define I18N_MSG_CMISC_011 11
-#define I18N_MSG_CMISC_012 11
+#define I18N_MSG_CMISC_012 12
 #define I18N_MSG_CMISC_013 13
 #define I18N_MSG_CMISC_014 14
 #define I18N_MSG_CMISC_015 15
