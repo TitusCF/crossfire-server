a20ff78d8cbe22a200dba18edbba7eb1eec034c4
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2009-01-21 06:22:25 +0000

    Whitespace changes: consistently reformat code.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@11194 282e977c-c81d-0410-88c4-b93c2d0d6712

--- devel/devel.c
+++ devel/devel.c
@@ -27,62 +27,64 @@
    that could be needed by an independent configure script) are available. The rest is available
    in config.h andd should be included in any plugin needing it.
 */
+
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+
 typedef struct
 {
     const char *name;
     const char *value;
 } cf_parameter;
-const cf_parameter cf_parameter_list[]=
-{
- {"CONFDIR",CONFDIR},
- {"DATADIR",DATADIR},
- {"LIBDIR",LIBDIR},
- {"LOCALDIR",LOCALDIR},
- {"PLUGIN_SUFFIX",PLUGIN_SUFFIX},
- };
-const int cf_parameter_list_size = sizeof (cf_parameter_list)/sizeof (cf_parameter);
+const cf_parameter cf_parameter_list[] = {
+    { "CONFDIR", CONFDIR },
+    { "DATADIR", DATADIR },
+    { "LIBDIR", LIBDIR },
+    { "LOCALDIR", LOCALDIR },
+    { "PLUGIN_SUFFIX", PLUGIN_SUFFIX },
+};
 
-int main (int argc, char **argv)
-{
+const int cf_parameter_list_size = sizeof(cf_parameter_list)/sizeof(cf_parameter);
+
+int main(int argc, char **argv) {
     int i;
-    if (argc==2) {
-      if (!strcmp (argv[1],"--parameter-list")) {
-          printf ("parameter maybe one of:\n");
-          printf ("\tPLUGININSTALLDIR\n");
-          for (i=0;i<cf_parameter_list_size;i++)
-              printf("\t%s\n",cf_parameter_list[i].name);
+
+    if (argc == 2) {
+      if (!strcmp(argv[1], "--parameter-list")) {
+          printf("parameter maybe one of:\n");
+          printf("\tPLUGININSTALLDIR\n");
+          for (i = 0; i < cf_parameter_list_size; i++)
+              printf("\t%s\n", cf_parameter_list[i].name);
           return 0;
       }
       /*Special case, handle plugin installation dir, which is most likeley why
         user wants to use crossfire-config in a configure script*/
-      if (!strcmp (argv[1],"PLUGININSTALLDIR")) {
-          printf ("%s/plugins/\n",LIBDIR);
+      if (!strcmp (argv[1], "PLUGININSTALLDIR")) {
+          printf("%s/plugins/\n", LIBDIR);
           return 0;
       }
-      if (!strcmp (argv[1],"--Dflags")) {
-          for (i=0;i<cf_parameter_list_size;i++)
-              printf("-D%s=\\\"%s\\\" ",cf_parameter_list[i].name,cf_parameter_list[i].value);
+      if (!strcmp(argv[1], "--Dflags")) {
+          for (i = 0; i < cf_parameter_list_size; i++)
+              printf("-D%s=\\\"%s\\\" ", cf_parameter_list[i].name, cf_parameter_list[i].value);
           /*printf ("-DDATADIR=\\\"%s\\\" -DLIBDIR=\\\"%s\\\" -DLOCALDIR=\\\"%s\\\"\n",
-                  cf_parameter_list[0].value,cf_parameter_list[1].value,cf_parameter_list[2].value);*/
+                  cf_parameter_list[0].value, cf_parameter_list[1].value, cf_parameter_list[2].value);*/
           printf("\n");
           return 0;
       }
-      for (i=0;i<cf_parameter_list_size;i++) {
-        if (!strcmp (argv[1],cf_parameter_list[i].name)) {
-          printf ("%s\n",cf_parameter_list[i].value);
+      for (i = 0; i < cf_parameter_list_size; i++) {
+        if (!strcmp(argv[1], cf_parameter_list[i].name)) {
+          printf ("%s\n", cf_parameter_list[i].value);
           return 0;
         }
       }
     }
     /* Bad arguments count or invalid ones */
-    printf ("usage: crossfire-config --Dflags");
-    printf (" (gives complete Dflags line for compiler invocation)\n");
-    printf ("usage: crossfire-config --parameter-list");
-    printf (" (show the list of available parameters)\n");
-    printf ("usage: crossfire-config <parameter name>");
-    printf (" (extract a compilation parameter)\n");
+    printf("usage: crossfire-config --Dflags");
+    printf(" (gives complete Dflags line for compiler invocation)\n");
+    printf("usage: crossfire-config --parameter-list");
+    printf(" (show the list of available parameters)\n");
+    printf("usage: crossfire-config <parameter name>");
+    printf(" (extract a compilation parameter)\n");
     return -1;
 }
