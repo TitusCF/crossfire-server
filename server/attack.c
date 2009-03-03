TitusCF/crossfire-server

commit f8b370d27608caa403784341292f1e02edfa73f2

server/attack.c: Fix for bug 1922771: xp non zero in meditation.  In     poison_living(), do some more work to find proper skill to set to credit     the exp gain - for players, op->skill doesn't look like it is set,     but op->chosen_skill is. MSW 2009-02-26
git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@11584 282e977c-c81d-0410-88c4-b93c2d0d6712

server/attack.c
@@ -2149,6 +2149,7 @@ int hit_player(object *op, int dam, object *hitter, uint32 type, int full_hit) {
 static void poison_living(object *op, object *hitter, int dam) {
     archetype *at = find_archetype("poisoning");
     object *tmp = present_arch_in_ob(at, op);
+    const char *skill;
 
     if (tmp == NULL) {
         if ((tmp = arch_to_object(at)) == NULL)
@@ -2169,10 +2170,13 @@ static void poison_living(object *op, object *hitter, int dam) {
                 tmp->stats.dam = dam;
 
             copy_owner(tmp, hitter);   /*  so we get credit for poisoning kills */
-            if (hitter->skill && hitter->skill != tmp->skill) {
+            skill = hitter->skill;
+            if (!skill) skill = hitter->chosen_skill->name;
+
+            if (skill && skill != tmp->skill) {
                 if (tmp->skill)
                     free_string(tmp->skill);
-                tmp->skill = add_refcount(hitter->skill);
+                tmp->skill = add_refcount(skill);
             }
 
             tmp->stats.food += dam;  /*  more damage, longer poisoning */

commit 32548d734b13982cfc78bfce5f6d3dba8851538f

server/attack.c: Fix crash bug introduced in r11584 (Fix for bug 1922771: xp non   zero in meditation). Bug triggered by charming chinese dragons.
git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@11592 282e977c-c81d-0410-88c4-b93c2d0d6712

server/attack.c
@@ -2171,7 +2171,7 @@ static void poison_living(object *op, object *hitter, int dam) {
 
             copy_owner(tmp, hitter);   /*  so we get credit for poisoning kills */
             skill = hitter->skill;
-            if (!skill) skill = hitter->chosen_skill->name;
+            if (!skill && hitter->chosen_skill) skill = hitter->chosen_skill->name;
 
             if (skill && skill != tmp->skill) {
                 if (tmp->skill)