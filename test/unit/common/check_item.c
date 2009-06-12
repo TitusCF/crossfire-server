cec207a0c19eee69509e65aa4e2316eafc76e5c4
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2009-06-12 11:38:01 +0000

    Fix unit test.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@11816 282e977c-c81d-0410-88c4-b93c2d0d6712

--- test/unit/common/check_item.c
+++ test/unit/common/check_item.c
@@ -112,7 +112,7 @@ START_TEST(test_describe_item) {
         "",
         "",
         "(normal movement)(see invisible)(spellcaster)(Spell abilities:)(burning hands ability)(medium fireball ability)(fear ability)(Attacks: physical)(resist fire +100)(resist cold -100)(resist confusion -100)(resist fear +100)(resist blindness +50)",
-        "(fast movement)(see invisible)(wield weapon)(archer)(wear armour)(wear ring)(read scroll)(spellcaster)(Spell abilities:)(paralyze)(fear ability)(small lightning)(large lightning)(slow ability)(resist magic +50)(resist electricity +100)(resist fear +100)",
+        "(fast movement)(see invisible)(wield weapon)(archer)(wear armour)(wear ring)(read scroll)(fires wand/rod/horn)(spellcaster)(Spell abilities:)(paralyze)(fear ability)(small lightning)(large lightning)(slow ability)(resist magic +50)(resist electricity +100)(resist fear +100)",
         "(speed +6)(item_power +6)(armour +3)",
         "(dam+3)(weapon speed 2)(Attacks: physical)",
         "(Con+2)(item_power +1)",
