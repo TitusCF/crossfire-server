f85500a89222504d5e87e40426d433aeda9a944f
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2009-06-17 21:40:24 +0000

    Make project compilable.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@11933 282e977c-c81d-0410-88c4-b93c2d0d6712

--- server/c_object.c
+++ server/c_object.c
@@ -379,7 +379,7 @@ static void pick_up_object(object *pl, object *op, object *tmp, int nrof) {
     if (tmp->inv)
         weight += tmp->carrying*(100-tmp->stats.Str)/100;
 
-    effective_weight_limit = MIN(get_weight_limit(pl->stats.Str, MAX_STAT);
+    effective_weight_limit = get_weight_limit(MIN(pl->stats.Str, MAX_STAT));
 
     if ((pl->weight+pl->carrying+weight) > effective_weight_limit) {
         draw_ext_info(0, 0, pl, MSG_TYPE_COMMAND, MSG_TYPE_COMMAND_FAILURE,
