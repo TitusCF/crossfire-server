fb886c99c9bd267fc1fb0d16fdc94fac6afd883c
ryo_saeba <ryo_saeba@282e977c-c81d-0410-88c4-b93c2d0d6712>
2010-06-20 14:58:26 +0000

    CRE: add 'exit' command, add status bar tips to menu commands.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@13422 282e977c-c81d-0410-88c4-b93c2d0d6712

--- utils/cre/CREMainWindow.cpp
+++ utils/cre/CREMainWindow.cpp
@@ -49,45 +49,59 @@ void CREMainWindow::closeEvent(QCloseEvent* event)
 void CREMainWindow::createActions()
 {
     myOpenArtifacts = new QAction(tr("Artifacts"), this);
+    myOpenArtifacts->setStatusTip(tr("List all defined artifacts."));
     connect(myOpenArtifacts, SIGNAL(triggered()), this, SLOT(onOpenArtifacts()));
 
     myOpenArchetypes = new QAction(tr("Archetypes"), this);
+    myOpenArchetypes->setStatusTip(tr("List all defined archetypes."));
     connect(myOpenArchetypes, SIGNAL(triggered()), this, SLOT(onOpenArchetypes()));
 
     myOpenTreasures = new QAction(tr("Treasures"), this);
+    myOpenTreasures->setStatusTip(tr("List all defined treasure lists."));
     connect(myOpenTreasures, SIGNAL(triggered()), this, SLOT(onOpenTreasures()));
 
     myOpenAnimations = new QAction(tr("Animations"), this);
+    myOpenAnimations->setStatusTip(tr("List all defined animations."));
     connect(myOpenAnimations, SIGNAL(triggered()), this, SLOT(onOpenAnimations()));
 
     myOpenFormulae = new QAction(tr("Formulae"), this);
+    myOpenFormulae->setStatusTip(tr("List all defined alchemy recipes."));
     connect(myOpenFormulae, SIGNAL(triggered()), this, SLOT(onOpenFormulae()));
 
     myOpenResources = new QAction(tr("Resources"), this);
+    myOpenResources->setStatusTip(tr("List all defined elements, except experience table."));
     connect(myOpenResources, SIGNAL(triggered()), this, SLOT(onOpenResources()));
 
     myOpenFaces = new QAction(tr("Faces"), this);
+    myOpenFaces->setStatusTip(tr("List all defined faces."));
     connect(myOpenFaces, SIGNAL(triggered()), this, SLOT(onOpenFaces()));
 
     myOpenMaps = new QAction(tr("Maps"), this);
+    myOpenMaps->setStatusTip(tr("List all maps, with their region."));
     connect(myOpenMaps, SIGNAL(triggered()), this, SLOT(onOpenMaps()));
 
     myOpenQuests = new QAction(tr("Quests"), this);
+    myOpenQuests->setStatusTip(tr("List all defined quests."));
     connect(myOpenQuests, SIGNAL(triggered()), this, SLOT(onOpenQuests()));
 
     myOpenMessages = new QAction(tr("Messages"), this);
+    myOpenMessages->setStatusTip(tr("List all message files."));
     connect(myOpenMessages, SIGNAL(triggered()), this, SLOT(onOpenMessages()));
 
     myOpenExperience = new QAction(tr("Experience"), this);
+    myOpenExperience->setStatusTip(tr("Display the experience table."));
     connect(myOpenExperience, SIGNAL(triggered()), this, SLOT(onOpenExperience()));
 
     mySaveFormulae = new QAction(tr("Formulae"), this);
+    mySaveFormulae->setEnabled(false);
     connect(mySaveFormulae, SIGNAL(triggered()), this, SLOT(onSaveFormulae()));
 
     mySaveQuests = new QAction(tr("Quests"), this);
+    mySaveQuests->setStatusTip(tr("Save all modified quests to disk."));
     connect(mySaveQuests, SIGNAL(triggered()), this, SLOT(onSaveQuests()));
 
     mySaveMessages = new QAction(tr("Messages"), this);
+    mySaveMessages->setStatusTip(tr("Save all modified messages."));
     connect(mySaveMessages, SIGNAL(triggered()), this, SLOT(onSaveMessages()));
 }
 
@@ -105,6 +119,10 @@ void CREMainWindow::createMenus()
     myOpenMenu->addAction(myOpenQuests);
     myOpenMenu->addAction(myOpenMessages);
     myOpenMenu->addAction(myOpenExperience);
+    myOpenMenu->addSeparator();
+    QAction* exit = myOpenMenu->addAction(tr("&Exit"));
+    exit->setStatusTip(tr("Close the application."));
+    connect(exit, SIGNAL(triggered()), this, SLOT(close()));
 
     mySaveMenu = menuBar()->addMenu(tr("&Save"));
     mySaveMenu->addAction(mySaveFormulae);
