03ae230170c85de322e5e79ecafe6f130be95036
ryo_saeba <ryo_saeba@282e977c-c81d-0410-88c4-b93c2d0d6712>
2010-09-26 17:40:13 +0000

    Add new client-server item flag for 'identified' status.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@13906 282e977c-c81d-0410-88c4-b93c2d0d6712

--- socket/item.c
+++ socket/item.c
@@ -99,6 +99,9 @@ static unsigned int query_flags(const object *op) {
     && ((op->env && op->env->container == op) || (!op->env && QUERY_FLAG(op, FLAG_APPLIED))))
         flags |= F_OPEN;
 
+    if (QUERY_FLAG(op, FLAG_IDENTIFIED))
+        flags |= F_IS_IDENTIFIED;
+
     if (QUERY_FLAG(op, FLAG_KNOWN_CURSED)) {
         if (QUERY_FLAG(op, FLAG_DAMNED))
             flags |= F_DAMNED;
