c3ba4d2287c849c0a595f8c78a327585effdaab7
ryo_saeba <ryo_saeba@282e977c-c81d-0410-88c4-b93c2d0d6712>
2011-06-15 18:51:27 +0000

    A few comments.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@14623 282e977c-c81d-0410-88c4-b93c2d0d6712

--- utils/cre/CREPrePostPanel.h
+++ utils/cre/CREPrePostPanel.h
@@ -10,11 +10,19 @@ class QComboBox;
 class QLineEdit;
 class QuestConditionScript;
 
+/**
+ * This panel is the 'pre' or 'post' subpanel in the messages panel.
+ */
 class CREPrePostPanel : public QWidget
 {
     Q_OBJECT
 
     public:
+        /**
+         * Standard constructor.
+         * @param scripts available script types for the conditions.
+         * @param parent ancestor of this panel.
+         */
         CREPrePostPanel(const QList<QuestConditionScript*> scripts, QWidget* parent);
         virtual ~CREPrePostPanel();
 
@@ -22,13 +30,19 @@ class CREPrePostPanel : public QWidget
         void setData(const QList<QStringList> data);
 
     signals:
+        /** Emitted when the data this panel manages was changed. */
         void dataModified();
 
     private:
+        /** Pre- or post- conditions we're working on. */
         QList<QStringList> myData;
+        /** The first item of each condition. */
         QListWidget* myItems;
+        /** Available conditions types. */
         QComboBox* myChoices;
+        /** For one condition, arguments to the script. */
         QListWidget* mySubItems;
+        /** Argument edit zone. */
         QLineEdit* myItemEdit;
 
     private slots:
--- utils/cre/CREReplyPanel.h
+++ utils/cre/CREReplyPanel.h
@@ -8,6 +8,9 @@ class QLineEdit;
 class QComboBox;
 class QTreeWidgetItem;
 
+/**
+ * This panel is the 'replies' subpanel in the messages panel.
+ */
 class CREReplyPanel : public QWidget
 {
     Q_OBJECT
@@ -20,16 +23,30 @@ class CREReplyPanel : public QWidget
         QList<QStringList> getData();
 
     signals:
+        /** Emitted when the data this panel manages changes .*/
         void dataModified();
 
     private:
+        /** The data this panel is working on. */
         QList<QStringList> myData;
+        /** Display for the replies. */
         QTreeWidget* myReplies;
+        /** Zone to edit the reply keyword. */
         QLineEdit* myText;
+        /** Zone to edit the message the NPC will say. */
         QLineEdit* myMessage;
+        /** Available reply type. */
         QComboBox* myType;
 
+        /**
+         * Fill a line of myReplies with the data.
+         * @param item line to fill.
+         * @param data contents to put in the line.
+         */
         void setText(QTreeWidgetItem* item, QStringList data);
+        /**
+         * Update the currently being edited reply with data in the various fields.
+         */
         void updateItem();
 
     private slots:
