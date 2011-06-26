20d3cd21dccbb6e06b048075e72f8745677518b3
ryo_saeba <ryo_saeba@282e977c-c81d-0410-88c4-b93c2d0d6712>
2011-06-26 10:11:04 +0000

    Update comments.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@14706 282e977c-c81d-0410-88c4-b93c2d0d6712

--- include/image.h
+++ include/image.h
@@ -29,21 +29,21 @@
 #ifndef IMAGE_H
 #define IMAGE_H
 
-/** Information about one image */
+/** Actual image data the client will display. */
 typedef struct face_info {
-    uint8 *data;            /**< image data */
-    uint16 datalen;         /**< length of the xpm data */
-    uint32 checksum;        /**< Checksum of face data */
+    uint8 *data;            /**< Image data. */
+    uint16 datalen;         /**< Length of data. */
+    uint32 checksum;        /**< Checksum of face data. */
 } face_info;
 
 /** Information about one face set */
 typedef struct {
-    char    *prefix;    /**< */
-    char    *fullname;
-    uint8   fallback;   /**< faceset to use when an image is not found in this faceset */
-    char    *size;
-    char    *extension;
-    char    *comment;
+    char    *prefix;    /**< Faceset short name, used in pictures names (base, clsc). */
+    char    *fullname;  /**< Full faceset name. */
+    uint8   fallback;   /**< Faceset to use when an image is not found in this faceset, index in facesets. */
+    char    *size;      /**< Human-readable set size. */
+    char    *extension; /**< Supplementary description. */
+    char    *comment;   /**< Human-readable comment for this set. */
     face_info   *faces; /**< images in this faceset */
 } face_sets;
 #define MAX_FACE_SETS   20  /**< Maximum number of image sets the program will handle */
