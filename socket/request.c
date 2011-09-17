126e813539c04c5dc4c95ab0c4f1c0e481458101
ryo_saeba <ryo_saeba@282e977c-c81d-0410-88c4-b93c2d0d6712>
2011-09-17 16:52:25 +0000

    Fix variable type.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@15096 282e977c-c81d-0410-88c4-b93c2d0d6712

--- socket/request.c
+++ socket/request.c
@@ -1662,7 +1662,7 @@ static void append_spell(player *pl, SockList *sl, object *spell) {
  */
 void esrv_add_spells(player *pl, object *spell) {
     SockList sl;
-    int size;
+    size_t size;
     sstring value;
 
     if (!pl) {
