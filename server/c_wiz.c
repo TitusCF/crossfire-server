c71f1672b90cebe72f35ae709c5fcf0c94335850
ryo_saeba <ryo_saeba@282e977c-c81d-0410-88c4-b93c2d0d6712>
2012-01-16 21:50:00 +0000

    Call the identify() method instead of settings flags.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@16682 282e977c-c81d-0410-88c4-b93c2d0d6712

--- server/c_wiz.c
+++ server/c_wiz.c
@@ -1107,8 +1107,7 @@ void command_create(object *op, const char *params) {
     if (art)
         give_artifact_abilities(tmp, art->item);
     if (need_identify(tmp)) {
-        SET_FLAG(tmp, FLAG_IDENTIFIED);
-        CLEAR_FLAG(tmp, FLAG_KNOWN_MAGICAL);
+        identify(tmp);
     }
 
     /*
