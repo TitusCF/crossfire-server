d8358bf8e1633737545681a58d217ee2799c8216
ryo_saeba <ryo_saeba@282e977c-c81d-0410-88c4-b93c2d0d6712>
2012-04-15 16:30:21 +0000

    Remove redundant header.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@17966 282e977c-c81d-0410-88c4-b93c2d0d6712

--- plugins/cfpython/cfpython_archetype.c
+++ plugins/cfpython/cfpython_archetype.c
@@ -28,7 +28,6 @@
 /*****************************************************************************/
 
 #include <cfpython.h>
-#include <cfpython_archetype_private.h>
 
 static PyObject *Crossfire_Archetype_GetName(Crossfire_Archetype *whoptr, void *closure) {
     return Py_BuildValue("s", cf_archetype_get_name(whoptr->arch));
@@ -106,3 +105,77 @@ static PyObject *Crossfire_Archetype_RichCompare(Crossfire_Archetype *left, Cros
     }
     return PyBool_FromLong(result);
 }
+
+static PyGetSetDef Archetype_getseters[] = {
+    { "Name",       (getter)Crossfire_Archetype_GetName,     NULL, NULL, NULL },
+    { "Next",       (getter)Crossfire_Archetype_GetNext,     NULL, NULL, NULL },
+    { "More",       (getter)Crossfire_Archetype_GetMore,     NULL, NULL, NULL },
+    { "Head",       (getter)Crossfire_Archetype_GetHead,     NULL, NULL, NULL },
+    { "Clone",      (getter)Crossfire_Archetype_GetClone,    NULL, NULL, NULL },
+    { NULL, NULL, NULL, NULL, NULL }
+};
+
+static PyMethodDef ArchetypeMethods[] = {
+    { "NewObject", (PyCFunction)Crossfire_Archetype_GetNewObject, METH_NOARGS, NULL },
+    { NULL, NULL, 0, NULL }
+};
+
+/* Our actual Python ArchetypeType */
+PyTypeObject Crossfire_ArchetypeType = {
+#ifdef IS_PY3K
+    /* See http://bugs.python.org/issue4385 */
+    PyVarObject_HEAD_INIT(NULL, 0)
+#else
+    PyObject_HEAD_INIT(NULL)
+    0,                         /* ob_size*/
+#endif
+    "Crossfire.Archetype",     /* tp_name*/
+    sizeof(Crossfire_Archetype), /* tp_basicsize*/
+    0,                         /* tp_itemsize*/
+    NULL,                      /* tp_dealloc*/
+    NULL,                      /* tp_print*/
+    NULL,                      /* tp_getattr*/
+    NULL,                      /* tp_setattr*/
+#ifdef IS_PY3K
+    NULL,                      /* tp_reserved */
+#else
+    (cmpfunc)Crossfire_Archetype_InternalCompare, /* tp_compare*/
+#endif
+    NULL,                      /* tp_repr*/
+    NULL,                      /* tp_as_number*/
+    NULL,                      /* tp_as_sequence*/
+    NULL,                      /* tp_as_mapping*/
+    PyObject_HashNotImplemented, /* tp_hash */
+    NULL,                      /* tp_call*/
+    NULL,                      /* tp_str*/
+    PyObject_GenericGetAttr,   /* tp_getattro*/
+    PyObject_GenericSetAttr,   /* tp_setattro*/
+    NULL,                      /* tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT|Py_TPFLAGS_BASETYPE, /* tp_flags*/
+    "Crossfire archetypes",    /* tp_doc */
+    NULL,                      /* tp_traverse */
+    NULL,                      /* tp_clear */
+    (richcmpfunc)Crossfire_Archetype_RichCompare, /* tp_richcompare */
+    0,                         /* tp_weaklistoffset */
+    NULL,                      /* tp_iter */
+    NULL,                      /* tp_iternext */
+    ArchetypeMethods,          /* tp_methods */
+    NULL,                      /* tp_members */
+    Archetype_getseters,       /* tp_getset */
+    NULL,                      /* tp_base */
+    NULL,                      /* tp_dict */
+    NULL,                      /* tp_descr_get */
+    NULL,                      /* tp_descr_set */
+    0,                         /* tp_dictoffset */
+    NULL,                      /* tp_init */
+    NULL,                      /* tp_alloc */
+    NULL,                      /* tp_new */
+    NULL,                      /* tp_free */
+    NULL,                      /* tp_is_gc */
+    NULL,                      /* tp_bases */
+    NULL,                      /* tp_mro */
+    NULL,                      /* tp_cache */
+    NULL,                      /* tp_subclasses */
+    NULL,                      /* tp_weaklist */
+    NULL,                      /* tp_del */
+};
