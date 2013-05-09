81b93d766a9fe25acaad031f19bc56af7453447a
ryo_saeba <ryo_saeba@282e977c-c81d-0410-88c4-b93c2d0d6712>
2013-05-09 17:47:47 +0000

    Correctly give artifact properties if the item is identified.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@18706 282e977c-c81d-0410-88c4-b93c2d0d6712

--- server/alchemy.c
+++ server/alchemy.c
@@ -446,6 +446,10 @@ static object *make_item_from_recipe(object *cauldron, const recipe *rp) {
         }
         transmute_materialname(item, art->item);
         give_artifact_abilities(item, art->item);
+        if (need_identify(item) && QUERY_FLAG(item, FLAG_IDENTIFIED)) {
+            /* artifacts are generated unidentified, so if the item is identified we must give it its real properties. */
+            object_give_identified_properties(item);
+        }
     }
     if (item->env != NULL)
         object_add_weight(cauldron, item->weight*(item->nrof != 0 ? item->nrof : 1));
