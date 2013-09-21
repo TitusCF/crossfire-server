b82235db99354e2cca939072127ec83ae399c5e7
partmedia <partmedia@282e977c-c81d-0410-88c4-b93c2d0d6712>
2013-09-21 23:56:34 +0000

    Rewrite standalone "random_map" program.
    This makes testing random map generators easier and lets us remove the
    already broken 'test.c' hack.
    
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@19062 282e977c-c81d-0410-88c4-b93c2d0d6712

--- random_maps/main.c
+++ random_maps/main.c
@@ -29,45 +29,138 @@
 
 #define LO_NEWFILE 2
 
-int main(int argc, char *argv[]) {
-    char InFileName[1024], OutFileName[1024];
-    mapstruct *newMap;
+static void generate_map(char *OutFileName) {
     RMParms rp;
-    FILE *fp;
+    mapstruct *newMap;
 
-    if (argc < 3) {
-        printf("\nUsage:  %s inputfile outputfile\n", argv[0]);
-        exit(0);
-    }
+    fprintf(stderr, "Reading parameters from stdin...\n");
 
-    strcpy(InFileName, argv[1]);
-    strcpy(OutFileName, argv[2]);
+    /* Initialize parameters and set initial settings. */
+    memset(&rp, 0, sizeof(RMParms));
+    rp.Xsize = -1;
+    rp.Ysize = -1;
 
+    /* Initialize Crossfire library. */
     init_globals();
     init_library();
     init_archetypes();
     init_artifacts();
     init_formulae();
     init_readable();
-
     init_gods();
-    memset(&rp, 0, sizeof(RMParms));
-    rp.Xsize = -1;
-    rp.Ysize = -1;
-    if ((fp = fopen(InFileName, "r")) == NULL) {
-        fprintf(stderr, "\nError: can not open %s\n", InFileName);
-        exit(1);
-    }
-    load_parameters(fp, LO_NEWFILE, &rp);
-    fclose(fp);
+
+    load_parameters(stdin, LO_NEWFILE, &rp);
+    fclose(stdin);
+
     newMap = generate_random_map(OutFileName, &rp, NULL);
     save_map(newMap, SAVE_MODE_INPLACE);
-    exit(0);
 }
 
-void set_map_timeout(mapstruct *oldmap)
-{
-    /* doesn't need to do anything */
+/**
+ * Print the human-readable layout of a map.
+ */
+static void print_map(char **layout, int width, int height) {
+    int i, j;
+
+    for (j = 0; j < height; j++) {
+        for (i = 0; i < width; i++) {
+            char display_char;
+            display_char = layout[i][j];
+
+            switch (display_char) {
+                case 0:
+                    display_char = '.';
+                    break;
+                case 'D':
+                    display_char = '+';
+                    break;
+            }
+
+            putchar(display_char);
+        }
+
+        putchar('\n');
+    }
+}
+
+/**
+ * Test the map layout produced by the specified generator.
+ * TODO: Select generator based on command-line arguments.
+ */
+static void test_layout(int width, int height) {
+    char **layout;
+    SRANDOM(time(0));
+
+    layout = roguelike_layout_gen(width, height, 0);
+    /*layout = make_snake_layout(width, height, 0); */
+    /*layout = make_square_spiral_layout(width, height, 0); */
+    /*layout = gen_corridor_rooms(width, height, 1); */
+    /*layout = maze_gen(width, height, 0); */
+    /*layout = map_gen_onion(width, height, 0, 0);*/
+
+    print_map(layout, width, height);
+}
+
+/** Print a message stating how to get help. */
+static void print_quickhelp() {
+    fprintf(stderr, "Type 'random_map -h' for usage instructions.\n");
+}
+
+/** Print out usage information. */
+static void print_usage() {
+    printf(
+        "Usage: random_map [options]\n"
+        "\n"
+        "Options:\n"
+        "  -h             display this help message\n"
+        "  -g <file>      randomly generate the specified map file\n"
+        "  -t             test map layout (overriden by -g)\n"
+        "  -x <width>     specify map width\n"
+        "  -y <height>    specify map height\n"
+    );
+}
+
+int main(int argc, char *argv[]) {
+    int flag, mode = 0, width = 80, height = 23;
+    char *filename_out;
+
+    /* Parse command-line arguments. */
+    while ((flag = getopt(argc, argv, "g:htx:y:")) != -1) {
+        switch (flag) {
+            case 'g':
+                mode = 2;
+                filename_out = optarg;
+                break;
+            case 'h':
+                print_usage();
+                exit(EXIT_SUCCESS);
+                break;
+            case 't':
+                mode = 1;
+                break;
+            case 'x':
+                width = atoi(optarg);
+                break;
+            case 'y':
+                height = atoi(optarg);
+                break;
+            case '?':
+                print_quickhelp();
+                exit(EXIT_FAILURE);
+                break;
+        }
+    }
+
+    if (mode == 1) {
+        test_layout(width, height);
+        exit(EXIT_SUCCESS);
+    } else if (mode == 2) {
+        generate_map(filename_out);
+        exit(EXIT_SUCCESS);
+    } else {
+        print_quickhelp();
+        exit(EXIT_FAILURE);
+    }
 }
 
 /* some plagarized code from apply.c--I needed just these two functions
@@ -160,18 +253,21 @@ void apply_auto_fix(mapstruct *m)
     FOR_MAP_FINISH();
 }
 
-/**
- * Those are dummy functions defined to resolve all symboles.
- * Added as part of glue cleaning.
- * Ryo 2005-07-15
- **/
-void draw_ext_info(int flags, int pri, const object *pl, uint8 type, uint8 subtype, const char *message)
-{
+/*
+ * The following dummy variables are only used to resolve symbols at compile
+ * time. They don't actually do anything useful.
+ */
+
+void set_map_timeout(mapstruct *oldmap) {
+}
+
+void draw_ext_info(int flags, int pri, const object *pl, uint8 type,
+        uint8 subtype, const char *message) {
     fprintf(logfile, "%s\n", message);
 }
 
-void draw_ext_info_format(int flags, int pri, const object *pl, uint8 type, uint8 subtype, const char *format, ...)
-{
+void draw_ext_info_format(int flags, int pri, const object *pl, uint8 type,
+        uint8 subtype, const char *format, ...) {
     va_list ap;
 
     va_start(ap, format);
@@ -180,62 +276,50 @@ void draw_ext_info_format(int flags, int pri, const object *pl, uint8 type, uint
 }
 
 
-void ext_info_map(int color, const mapstruct *map, uint8 type, uint8 subtype, const char *str1)
-{
+void ext_info_map(int color, const mapstruct *map, uint8 type, uint8 subtype,
+        const char *str1) {
     fprintf(logfile, "ext_info_map: %s\n", str1);
 }
 
-void move_firewall(object *ob)
-{
+void move_firewall(object *ob) {
 }
 
-void emergency_save(int x)
-{
+void emergency_save(int x) {
 }
 
-void clean_tmp_files(void)
-{
+void clean_tmp_files() {
 }
 
-void esrv_send_item(object *ob, object *obx)
-{
+void esrv_send_item(object *ob, object *obx) {
 }
 
-void esrv_update_item(int flags, object *pl, object *op)
-{
+void esrv_update_item(int flags, object *pl, object *op) {
 }
 
-void dragon_ability_gain(object *ob, int x, int y)
-{
+void dragon_ability_gain(object *ob, int x, int y) {
 }
 
-void set_darkness_map(mapstruct *m)
-{
+void set_darkness_map(mapstruct *m) {
 }
 
-object *find_skill_by_number(object *who, int skillno)
-{
+object *find_skill_by_number(object *who, int skillno) {
     return NULL;
 }
 
-void esrv_del_item(player *pl, object *ob)
-{
+void esrv_del_item(player *pl, object *ob) {
 }
 
-void esrv_update_spells(player *pl)
-{
+void esrv_update_spells(player *pl) {
 }
 
-void rod_adjust(object *rod)
-{
+void rod_adjust(object *rod) {
 }
 
-int execute_event(object *op, int eventcode, object *activator, object *third, const char *message, int fix)
-{
+int execute_event(object *op, int eventcode, object *activator, object *third,
+        const char *message, int fix) {
     return 0;
 }
 
-int execute_global_event(int eventcode, ...)
-{
+int execute_global_event(int eventcode, ...) {
     return 0;
 }
