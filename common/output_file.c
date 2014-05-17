5880f6097551db9a28f1f1653a1af0f792d81f26
akirschbaum <akirschbaum@282e977c-c81d-0410-88c4-b93c2d0d6712>
2014-05-17 19:57:35 +0000

    Add files missing from r19434.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@19435 282e977c-c81d-0410-88c4-b93c2d0d6712

---
+++ common/output_file.c
@@ -0,0 +1,99 @@
+/*
+ * Crossfire -- cooperative multi-player graphical RPG and adventure game
+ *
+ * Copyright (c) 1999-2014 Mark Wedel and the Crossfire Development Team
+ * Copyright (c) 1992 Frank Tore Johansen
+ *
+ * Crossfire is free software and comes with ABSOLUTELY NO WARRANTY. You are
+ * welcome to redistribute it under certain conditions. For details, please
+ * see COPYING and LICENSE.
+ *
+ * The authors can be reached via e-mail at <crossfire@metalforge.org>.
+ */
+
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "global.h"
+#include "logger.h"
+#include "output_file.h"
+
+
+/**
+ * The extension for temporary files that is appended to the original output
+ * filename during write operations.
+ */
+#define TMP_EXT ".tmp"
+
+
+FILE *of_open(OutputFile *of, const char *fname) {
+    char *fname_tmp;
+    FILE *f;
+    char tmp[1024];
+
+    fname_tmp = malloc(strlen(fname)+sizeof(TMP_EXT));
+    if (fname_tmp == NULL) {
+        LOG(llevError, "%s: out of memory\n", fname);
+        return NULL;
+    }
+
+    sprintf(fname_tmp, "%s%s", fname, TMP_EXT);
+    remove(fname_tmp);
+    f = fopen(fname_tmp, "w");
+    if (f == NULL) {
+        LOG(llevError, "%s: %s\n", fname_tmp, strerror_local(errno, tmp, sizeof(tmp)));
+        free(fname_tmp);
+        return NULL;
+    }
+
+    of->fname = strdup_local(fname);
+    if (of->fname == NULL) {
+        LOG(llevError, "%s: out of memory\n", fname);
+        free(fname_tmp);
+        fclose(f);
+        return NULL;
+    }
+    of->fname_tmp = fname_tmp;
+    of->file = f;
+    return f;
+}
+
+int of_close(OutputFile *of)
+{
+    char tmp[1024];
+
+    if (ferror(of->file)) {
+        LOG(llevError, "%s: write error\n", of->fname);
+        fclose(of->file);
+        remove(of->fname_tmp);
+        free(of->fname_tmp);
+        free(of->fname);
+        return 0;
+    }
+    if (fclose(of->file) != 0) {
+        LOG(llevError, "%s: %s\n", of->fname, strerror_local(errno, tmp, sizeof(tmp)));
+        remove(of->fname_tmp);
+        free(of->fname_tmp);
+        free(of->fname);
+        return 0;
+    }
+    if (rename(of->fname_tmp, of->fname) != 0) {
+        LOG(llevError, "%s: cannot rename from %s: %s\n", of->fname, of->fname_tmp, strerror_local(errno, tmp, sizeof(tmp)));
+        remove(of->fname_tmp);
+        free(of->fname_tmp);
+        free(of->fname);
+        return 0;
+    }
+    free(of->fname_tmp);
+    free(of->fname);
+    return 1;
+}
+
+void of_cancel(OutputFile *of)
+{
+    fclose(of->file);
+    remove(of->fname_tmp);
+    free(of->fname_tmp);
+    free(of->fname);
+}
---
+++ include/output_file.h
@@ -0,0 +1,98 @@
+/*
+ * Crossfire -- cooperative multi-player graphical RPG and adventure game
+ *
+ * Copyright (c) 1999-2014 Mark Wedel and the Crossfire Development Team
+ * Copyright (c) 1992 Frank Tore Johansen
+ *
+ * Crossfire is free software and comes with ABSOLUTELY NO WARRANTY. You are
+ * welcome to redistribute it under certain conditions. For details, please
+ * see COPYING and LICENSE.
+ *
+ * The authors can be reached via e-mail at <crossfire@metalforge.org>.
+ */
+
+/**
+ * @file
+ * Functions for creating text output files. During (re-)writing a new file or
+ * if a write error occurs the old file contents are still available.
+ *
+ * Example usage:
+ * <code>
+ * <pre>
+ * OutputFile of;
+ * FILE *f;
+ * f = of_open(&of, "some_file.txt", llevError);
+ * if (f == NULL)
+ *     return;
+ * fprintf(f, "...");
+ * fprintf(f, "...");
+ * if (!of_close(&of))
+ *     return;
+ * </pre>
+ * </code>
+ */
+
+#ifndef OUTPUT_FILE_H
+#define OUTPUT_FILE_H
+
+#include <stdio.h>
+
+
+typedef struct
+{
+    /**
+     * The original output file that is written.
+     */
+    char *fname;
+
+    /**
+     * The temporary output file that is written to until #of_close() is
+     * called.
+     */
+    char *fname_tmp;
+
+    /**
+     * The file pointer for fname_tmp.
+     */
+    FILE *file;
+} OutputFile;
+
+
+/**
+ * Opens an output file. It is equivalent to <code>fopen(fname, "w");</code>
+ * but uses a temporary file instead of <code>fname</code> until #of_close() is
+ * called.
+ *
+ * @param of an #OutputFile instance allocated by the caller; it must not be
+ * freed or reused until #of_close() has been called
+ *
+ * @param fname the filename to open; the string may be freed or reused
+ * afterwards
+ *
+ * @return the file pointer or <code>NULL</code> if the file could not be
+ * opened; in this case an error message has been printed and the original file
+ * content are unchanged
+ */
+FILE *of_open(OutputFile *of, const char *fname);
+
+/**
+ * Closes an output file. This function (or #of_cancel()} must be called
+ * exactly once for each successful call to #of_open().
+ *
+ * @param of the output file instance to operate on
+ *
+ * @return whether writing was successful; if unsuccessful an error message has
+ * been printed and the original file content are unchanged
+ */
+int of_close(OutputFile *of);
+
+/**
+ * Cancels a save process. This closes and deletes the temporary file, leaving
+ * the original file contents unmodified. This function (or #on_close()} must
+ * be called exactly once for each successful call to #op_open().
+ *
+ * @param of the output file instance to operate on
+ */
+void of_cancel(OutputFile *of);
+
+#endif
