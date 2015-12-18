14305454e6533326c5250ebbcc254e4fcbc720cb
silvernexus <silvernexus@282e977c-c81d-0410-88c4-b93c2d0d6712>
2015-12-18 23:22:57 +0000

    Replaced spell-summary.ps with equivalent text file and PostScript generation command.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@20185 282e977c-c81d-0410-88c4-b93c2d0d6712

--- doc/spell-docs/create_ps.sh
+++ doc/spell-docs/create_ps.sh
@@ -14,6 +14,7 @@ then
 	enscript -2 -b "Crossfire Prayers" -p "prayer-list.ps" -f "ZapfChancery-MediumItalic12" -F "ZapfChancery-MediumItalic13" prayer-list.txt
 	enscript -B -p "spell-info.ps" -f "Courier-BoldOblique12" --margins=40:40:30:30 spell-info.txt
         enscript -B -p "tome-of-magic.ps" -f "Palatino-Italic12" -T 14 --margins=40:14:30:30 tome-of-magic.txt
+	enscript -B -p "spell-summary.ps" -f "Courier8" --margins=40:18:34:30 spell-summary.txt
 else
 	echo "Enscript not found, PostScript generation will be skipped.";
 fi
