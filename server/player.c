29b874415076e8772ba2037e3d3f17be183056cb
Kevin Zheng <kevinz5000@gmail.com>
2019-02-18 11:13:40 -0800

    Trigger player death event on permadeath only

--- server/player.c
+++ server/player.c
@@ -3609,8 +3609,6 @@ void kill_player(object *op, const object *killer) {
     if (execute_event(op, EVENT_DEATH, NULL, NULL, NULL, SCRIPT_FIX_ALL) != 0)
         return;
 
-    /* Lauwenmark: Handle for the global death event */
-    execute_global_event(EVENT_PLAYER_DEATH, op, killer);
     if (op->stats.food < 0) {
         snprintf(buf, sizeof(buf), "%s starved to death.", op->name);
         strcpy(op->contr->killer, "starvation");
@@ -3623,6 +3621,7 @@ void kill_player(object *op, const object *killer) {
 
     switch(settings.not_permadeth) {
         case 0:
+            execute_global_event(EVENT_PLAYER_DEATH, op, killer);
             kill_player_permadeath(op);
             break;
         case 1:
