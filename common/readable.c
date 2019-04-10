b2d49adb0afb16ac5c63f7ddff0a899dfde322e9
Kevin Zheng <kevinz5000@gmail.com>
2019-04-10 14:26:25 -0700

    Remove all but alchemy random texts

--- common/readable.c
+++ common/readable.c
@@ -2081,6 +2081,7 @@ void tailor_readable_ob(object *book, int msg_type) {
 
     msg_type = msg_type > 0 ? msg_type : (int)(RANDOM()%6);
     switch (msg_type) {
+#if 0
     case MSGTYPE_MONSTER:
         message = mon_info_msg(level, book_buf_size, book);
         break;
@@ -2092,6 +2093,7 @@ void tailor_readable_ob(object *book, int msg_type) {
     case MSGTYPE_SPELLPATH: /* grouping incantations/prayers by path */
         message = spellpath_msg(level, book_buf_size, NULL);
         break;
+#endif
 
     case MSGTYPE_ALCHEMY: /* describe an alchemy formula */
         make_formula_book(book, level);
@@ -2099,9 +2101,11 @@ void tailor_readable_ob(object *book, int msg_type) {
         return;
         break;
 
+#if 0
     case MSGTYPE_GODS: /* bits of information about a god */
         message = god_info_msg(level, book_buf_size, book);
         break;
+#endif
 
     case MSGTYPE_LIB: /* use info list in lib/ */
     default:
