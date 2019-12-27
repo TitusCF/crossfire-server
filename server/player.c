2eacdc4a365c4d3a894b1208a89edf4279915adc
partmedia <partmedia@282e977c-c81d-0410-88c4-b93c2d0d6712>
2019-12-27 01:33:33 +0000

    Correctly find default language for new players
    The default language for new players should be set to -1, not 0, in
    order for the server to correctly set the language to the default.
    
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@21059 282e977c-c81d-0410-88c4-b93c2d0d6712

--- server/player.c
+++ server/player.c
@@ -359,7 +359,7 @@ player *get_player(player *p) {
     p->peaceful = 1;   /* default peaceful */
     p->do_los = 1;
     p->no_shout = 0;   /* default can shout */
-    p->language = 0;
+    p->language = -1;  // find default language
     p->unarmed_skill = NULL;
     p->ticks_played = 0;
 
