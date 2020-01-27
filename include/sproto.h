ffeb1a6d48716fa89f9325c4698f46c8fd533aad
TitusCF <46248632+TitusCF@users.noreply.github.com>
2020-01-26 21:13:25 -0500

    Big pile of code changes from Titus

--- include/sproto.h
+++ include/sproto.h
@@ -23,12 +23,6 @@ Account_Char *account_char_remove(Account_Char *chars, const char *pl_name);
 void account_char_free(Account_Char *chars);
 /* alchemy.c */
 int use_alchemy(object *op);
-/* smithery.c */
-int use_smithery(object *op);
-/* bowery.c */
-int use_bowery(object *op);
-/* jewler.c */
-int use_jeweler(object *op);
 /* apply.c */
 int transport_can_hold(const object *transport, const object *op, int nrof);
 int should_director_abort(const object *op, const object *victim);
