5e389f90d08d4a571e4e56387eb25505b18d67f7
Kevin Zheng <kevinz5000@gmail.com>
2020-08-16 16:26:58 -0700

    Add server dependency on SQLite3

--- include/server.h
+++ include/server.h
@@ -2,8 +2,10 @@
 #define _SERVER_H
 
 #include <signal.h>
+#include <sqlite3.h>
 
 extern volatile sig_atomic_t shutdown_flag;
+extern sqlite3* server_db;
 
 void player_map_change_common(object* op, mapstruct* const oldmap,
                               mapstruct* const newmap);
