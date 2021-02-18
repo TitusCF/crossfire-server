64b5fbc23291c979ff06852ad76d71c83f023340
ryo_saeba <ryo_saeba@282e977c-c81d-0410-88c4-b93c2d0d6712>
2021-02-18 17:07:40 +0000

    Add copyright information.
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/server/trunk@21983 282e977c-c81d-0410-88c4-b93c2d0d6712

--- include/AssetsManager.h
+++ include/AssetsManager.h
@@ -1,3 +1,15 @@
+/*
+ * Crossfire -- cooperative multi-player graphical RPG and adventure game
+ *
+ * Copyright (c) 2020-2021 the Crossfire Development Team
+ *
+ * Crossfire is free software and comes with ABSOLUTELY NO WARRANTY. You are
+ * welcome to redistribute it under certain conditions. For details, please
+ * see COPYING and LICENSE.
+ *
+ * The authors can be reached via e-mail at <crossfire@metalforge.org>.
+ */
+
 #ifndef ASSETSMANAGER_H
 #define ASSETSMANAGER_H
 
