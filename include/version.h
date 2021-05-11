cc15c04d6d2d6b8e37c0111417a17d9426307755
Kevin Zheng <kevinz5000@gmail.com>
2021-05-10 18:11:15 -0700

    Fix Git version string

--- include/version.h
+++ include/version.h
@@ -1,7 +1,7 @@
 #include <svnversion.h>  /* Automatically created at build time.*/
 
 #ifdef SVN_REV
-#define FULL_VERSION VERSION"-r"SVN_REV
+#define FULL_VERSION SVN_REV
 #else
 #define FULL_VERSION VERSION
 #endif
