6d72c6d29b940ba682ae5d19e2569920c85c9ed6
Nicolas Weeger <ryo_saeba@users.sourceforge.net>
2021-07-30 18:08:33 +0200

    Fix fields type

--- include/account_char.h
+++ include/account_char.h
@@ -24,13 +24,13 @@
  * One character account.
  */
 typedef struct account_char_struct {
-    sstring *name;            /** < Name of this character/player */
-    sstring *character_class; /** < Class of this character */
-    sstring *race;            /** < Race of this character */
+    sstring name;             /** < Name of this character/player */
+    sstring character_class;  /** < Class of this character */
+    sstring race;             /** < Race of this character */
     uint8_t level;            /** < Level of this character */
-    sstring *face;            /** < Face of this character */
-    sstring *party;           /** < Character this party belonged to */
-    sstring *map;             /** < Last map this character was on */
+    sstring face;             /** < Face of this character */
+    sstring party;            /** < Character this party belonged to */
+    sstring map;              /** < Last map this character was on */
     uint8_t isDead;           /** < Should stay at zero if alive, anything else if dead (hopefully 1, but doesn't have to be) */
     struct account_char_struct  *next;
 } Account_Char;
