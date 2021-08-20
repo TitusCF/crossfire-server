be01694ba678d8219a0286cb4b7788ee6789fc97
Nicolas Weeger <ryo_saeba@users.sourceforge.net>
2021-08-20 20:33:18 +0200

    Under Windows, force CRE to use its directory as base for share, etc and var

--- utils/cre/cre.cpp
+++ utils/cre/cre.cpp
@@ -12,6 +12,11 @@
 #include "CREReportDefinition.h"
 #include "CREReportDefinitionManager.h"
 
+#ifdef WIN32
+extern "C" {
+#include "global.h"
+}
+#endif
 
 int main(int argc, char **argv) {
     QCoreApplication::setOrganizationName("The Legendary Team of Ailesse");
@@ -23,6 +28,21 @@ int main(int argc, char **argv) {
     qRegisterMetaTypeStreamOperators<CREFilterDefinitionManager>("CREFilterDefinitionManager");
     qRegisterMetaTypeStreamOperators<CREReportDefinition>("CREReportDefinition");
     qRegisterMetaTypeStreamOperators<CREReportDefinitionManager>("CREReportDefinitionManager");
+
+#ifdef WIN32
+    // Application compiled with "--prefix=", somehow the root is then c:\, which is bad
+    // So force directories to point to the exe's path and something else.
+    std::string confdir = app.applicationDirPath().toStdString() + "/etc";
+    settings.confdir = confdir.c_str();
+    std::string localdir = app.applicationDirPath().toStdString() + "/var";
+    settings.localdir = localdir.c_str();
+    std::string datadir = app.applicationDirPath().toStdString() + "/share";
+    settings.datadir = datadir.c_str();
+    std::string temp = QDir::tempPath().toStdString();
+    settings.tmpdir = temp.c_str();
+    // Force log level to be debug to see all output
+    settings.debug = llevDebug;
+#endif
 
     CRESettings settings;
     if (!settings.ensureOptions())
