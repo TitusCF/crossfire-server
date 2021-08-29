ae0be692f543335c152922058266ca4a65725a42
Nicolas Weeger <ryo_saeba@users.sourceforge.net>
2021-08-29 12:16:12 +0200

    Basic licenses report, to find invalid filenames

--- utils/cre/LicenseManager.h
+++ utils/cre/LicenseManager.h
@@ -19,24 +19,56 @@
 
 struct BufferReader;
 
+/**
+ * Contains all information about licenses for faces.
+ */
 class LicenseManager {
 public:
 
-  typedef std::pair<std::string, std::string> LicenseItem;
-  typedef std::map<std::string, std::vector<LicenseItem>> LicenseItems;
+  typedef std::pair<std::string, std::string> LicenseItem;              /**< One line in a license file. */
+  typedef std::map<std::string, std::vector<LicenseItem>> LicenseItems; /**< For a single face, map between a faceset's prefix and the license items. */
 
+  /** Standard constructor. */
   LicenseManager();
+  /** Standard destructor. */
   virtual ~LicenseManager();
 
+  /**
+   * Get license information for a face.
+   * @param face face name, with the suffix.
+   * @return license information.
+   */
   LicenseItems getForFace(const std::string &face);
 
+  /**
+   * Get all known license information.
+   * @return map between face names without prefix and license information.
+   */
+  std::map<std::string, LicenseItems> getAll() const { return m_licenses; }
+
+  /**
+   * Process a license file.
+   * @param reader source.
+   * @param filename full file name.
+   */
   static void readLicense(BufferReader *reader, const char *filename);
+
+  /**
+   * Get the single instance of the LicenseManager.
+   * @return instance.
+   */
   static LicenseManager *get();
 
-private:
+  /**
+   * Get the expected license name from a face name.
+   * @param face face name, with the suffix.
+   * @return expected license file name, empty if face is invalid.
+   */
+  static std::string licenseNameFromFaceName(const std::string &face);
 
-  static LicenseManager *instance;
-  std::map<std::string, LicenseItems> m_licenses;
+private:
+  static LicenseManager *instance;                /**< Single instance. */
+  std::map<std::string, LicenseItems> m_licenses; /**< License information for all faces. */
 };
 
 #endif /* LICENSEMANAGER_H */
