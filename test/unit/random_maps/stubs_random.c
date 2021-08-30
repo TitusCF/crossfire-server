af3a88140fbff2ecd1a82152743a72347a51a9c6
Nicolas Weeger <ryo_saeba@users.sourceforge.net>
2021-08-30 22:52:30 +0200

    Fix unused parameter warnings

--- test/unit/random_maps/stubs_random.c
+++ test/unit/random_maps/stubs_random.c
@@ -8,39 +8,59 @@
 #include <sproto.h>
 
 void apply_auto_fix(mapstruct *m) {
+    (void)m;
 }
 
 void draw_ext_info(int flags, int pri, const object *pl, uint8_t type, uint8_t subtype, const char *message) {
+    (void)flags;
+    (void)pri;
+    (void)pl;
+    (void)type;
+    (void)subtype;
     fprintf(logfile, "%s\n", message);
 }
 
 void move_firewall(object *ob) {
+    (void)ob;
 }
 
 void emergency_save(int x) {
+    (void)x;
 }
 
 void clean_tmp_files(void) {
 }
 
 void esrv_send_item(object *ob, object *obx) {
+    (void)ob;
+    (void)obx;
 }
 
 void dragon_ability_gain(object *ob, int x, int y) {
+    (void)ob;
+    (void)x;
+    (void)y;
 }
 
 void set_darkness_map(mapstruct *m) {
+    (void)m;
 }
 
 object *find_skill_by_number(object *who, int skillno) {
+    (void)who;
+    (void)skillno;
     return NULL;
 }
 
 void esrv_del_item(player *pl, object *ob) {
+    (void)pl;
+    (void)ob;
 }
 
 void esrv_update_spells(player *pl) {
+    (void)pl;
 }
 
 void rod_adjust(object *rod) {
+    (void)rod;
 }
