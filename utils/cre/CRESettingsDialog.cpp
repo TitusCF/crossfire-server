692839ffa071b482c709f39a6005947128731711
Nicolas Weeger <ryo_saeba@users.sourceforge.net>
2021-10-19 20:32:40 +0200

    Rename label, and add a description

--- utils/cre/CRESettingsDialog.cpp
+++ utils/cre/CRESettingsDialog.cpp
@@ -10,13 +10,16 @@ CRESettingsDialog::CRESettingsDialog(CRESettings* settings)
 
     int line = 0;
 
-    layout->addWidget(new QLabel(tr("Map cache directory:"), this), line, 0);
+    layout->addWidget(new QLabel(tr("Cache directory:"), this), line, 0);
     myMapCache = new QLineEdit(this);
     myMapCache->setText(settings->mapCacheDirectory());
     if (myMapCache->text().isEmpty())
         myMapCache->setText(QStandardPaths::standardLocations(QStandardPaths::DataLocation)[0]);
     layout->addWidget(myMapCache, line, 1);
 
+    line++;
+
+    layout->addWidget(new QLabel(tr("This directory is used to store information between program runs.\nIt should be persistent (don't put in a temporary directory cleaned now and then),\nbut data can be recreated if needed."), this), line, 0, 1, 2);
     line++;
 
     QDialogButtonBox* buttons = new QDialogButtonBox(QDialogButtonBox::Save | QDialogButtonBox::Cancel, Qt::Horizontal, this);
